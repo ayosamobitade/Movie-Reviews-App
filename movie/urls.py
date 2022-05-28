dacfd15847f685694a4a81cd7830f7fc11c08b13
UncleAyo <ayosamobitade@gmail.com>
2022-05-28 10:24:03 +0100

    working on the movie app

--- movie/urls.py
+++ movie/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MovieView
+from .views import MovieView, home
 
 
 urlpatterns = [
     path('movie', MovieView, name = "home"),
+    path('', home, name = 'homepage')
 
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, "movie/home.html")
+
+def MovieView(request):
+    return render(request, 'home.html')
